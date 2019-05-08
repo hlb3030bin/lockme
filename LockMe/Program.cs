2905a3e8ee92f18353accd60d34168a752ae2c39
huanglb <hlb@fastfish.com.cn>
2019-05-08 18:02:03 +0800

    提交

--- LockMe/Program.cs
+++ LockMe/Program.cs
@@ -19,51 +19,51 @@ namespace LockMe
             //Application.Run(new LockMe());
             SysAdminRunApp.RunApp(new LockMe());
             return;
-            #region 去掉
-            //获得当前登录的Windows用户标示
-            System.Security.Principal.WindowsIdentity identity = System.Security.Principal.WindowsIdentity.GetCurrent();
-            System.Security.Principal.WindowsPrincipal principal =
-                new System.Security.Principal.WindowsPrincipal(identity);
+            //#region 去掉
+            ////获得当前登录的Windows用户标示
+            //System.Security.Principal.WindowsIdentity identity = System.Security.Principal.WindowsIdentity.GetCurrent();
+            //System.Security.Principal.WindowsPrincipal principal =
+            //    new System.Security.Principal.WindowsPrincipal(identity);
 
-            logger.Info("------判断是否管理员------");
-            //判断当前登录用户是否为管理员
-            if (principal.IsInRole(System.Security.Principal.WindowsBuiltInRole.Administrator))
-            {
-                logger.Info("------是管理员------");
-                logger.Info("------ Application.Run(new LockMe());------");
+            //logger.Info("------判断是否管理员------");
+            ////判断当前登录用户是否为管理员
+            //if (principal.IsInRole(System.Security.Principal.WindowsBuiltInRole.Administrator))
+            //{
+            //    logger.Info("------是管理员------");
+            //    logger.Info("------ Application.Run(new LockMe());------");
 
-                //如果是管理员，则直接运行
-                Application.Run(new LockMe());
-            }
-            else
-            {
-                //创建启动对象
-                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
-                startInfo.UseShellExecute = true;
-                startInfo.WorkingDirectory = Environment.CurrentDirectory;
-                startInfo.FileName = Application.ExecutablePath;
-                //设置启动动作,确保以管理员身份运行
-                startInfo.Verb = "runas";
-                try
-                {
-                    logger.Info("------管理员方式执行------");
-                    logger.Info("------System.Diagnostics.Process.Start(startInfo);------");
-                    System.Diagnostics.Process.Start(startInfo);
-                }
-                catch (Exception ex)
-                {
-                    logger.Info("------异常结束------");
-                    logger.Info($@"------{ex.Message}------");
-                    logger.Info($@"------{ex.StackTrace}------");
-                    return;
-                }
+            //    //如果是管理员，则直接运行
+            //    Application.Run(new LockMe());
+            //}
+            //else
+            //{
+            //    //创建启动对象
+            //    System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            //    startInfo.UseShellExecute = true;
+            //    startInfo.WorkingDirectory = Environment.CurrentDirectory;
+            //    startInfo.FileName = Application.ExecutablePath;
+            //    //设置启动动作,确保以管理员身份运行
+            //    startInfo.Verb = "runas";
+            //    try
+            //    {
+            //        logger.Info("------管理员方式执行------");
+            //        logger.Info("------System.Diagnostics.Process.Start(startInfo);------");
+            //        System.Diagnostics.Process.Start(startInfo);
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        logger.Info("------异常结束------");
+            //        logger.Info($@"------{ex.Message}------");
+            //        logger.Info($@"------{ex.StackTrace}------");
+            //        return;
+            //    }
 
-                logger.Info("------ Application.Exit();------");
+            //    logger.Info("------ Application.Exit();------");
 
-                //退出
-                Application.Exit();
-                //return;             
-            }
+            //    //退出
+            //    Application.Exit();
+            //    //return;             
+            //}
             #endregion
         }
     }
